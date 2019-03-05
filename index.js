6d86a7b68259d6e2e723f76e8bd54547fffc9ef2
Lauren <laurenribeiro06@gmail.com>
2019-03-05 19:06:58 +0000

    Updated task object to contain methods instead of having functions outside of the object

--- index.js
+++ index.js
@@ -9,21 +9,19 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+
+    logState: function(){
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function (){
+      this.complete = true;
+    }
   };
   return task;
 }
 
-
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(task) {
-  task.complete = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-}
 
 // DRIVER CODE BELOW
 
@@ -34,11 +32,9 @@ const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
 
-logTaskState(task1); // Clean Cat Litter has not been completed
+task1.logState(); // Clean Cat Litter has not been completed
 
-completeTask(task1);
+task1.markCompleted();
 
-logTaskState(task1); // Clean Cat Litter has been completed
-
-console.log(tasks);
+task1.logState(); // Clean Cat Litter has been completed
 
